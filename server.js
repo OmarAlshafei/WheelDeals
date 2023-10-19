fa7cb2db06e9acac803b277efdd6287a508a20a4
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2023-10-19 08:32:29 -0400

    Updated APIs

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 const app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 9000))
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 const url = process.env.MONGODB_URL;
@@ -23,20 +23,10 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    next();
-  });
-  
 // app.listen(5000); // start Node + Express server on port 5000
 app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT);
 });
-
-
-
 
 // Checks if password meets requirements
 function isComplex(password) {
@@ -97,9 +87,9 @@ app.post('/api/login', async (req, res, next) => {
     // incoming: login, password
     // outgoing: id, firstName, lastName, error
     var error = '';
-    const { User, Pass } = req.body;
+    const { userName, password } = req.body;
     const db = client.db('cop4331');
-    const results = await db.collection('Users').find({ userName: User, password: Pass }).toArray();
+    const results = await db.collection('Users').find({ userName: userName, password: password }).toArray();
     var id = -1;
     var fn = '';
     var ln = '';
