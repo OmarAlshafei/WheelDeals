b2b12ab6b14425c306c5ebcc5b5171d02cad62d6
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-10-19 22:47:58 -0400

    create homepage,navbar,slideshow,table demo and update the app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import LoginPage from './pages/LoginPage';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
+        <Route path="/">
+          <HomePage />
+          {/* <LoginPage /> */}
+        </Route>
+        <Route path="/signin">
           <LoginPage />
         </Route>
       </Switch>
