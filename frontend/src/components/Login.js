38b1b78f71e213e9ca71ffe657345c9fe2458e83
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-10-21 14:55:58 -0400

    create register and add switch function between login and register

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,61 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function Login() {
-  const app_name = 'wheeldeals-d3e9615ad014'
-  function buildPath(route)
-  {
-    if (process.env.NODE_ENV === 'production')
-    {
-      return 'https://' + app_name + '.herokuapp.com/' + route;
-    }
-    else
-    {
-      return 'http://localhost:5000/' + route;
+const Login = (props) => {
+  const app_name = "wheeldeals-d3e9615ad014";
+  function buildPath(route) {
+    if (process.env.NODE_ENV === "production") {
+      return "https://" + app_name + ".herokuapp.com/" + route;
+    } else {
+      return "http://localhost:5000/" + route;
     }
   }
-  
+
   var loginName;
   var loginPassword;
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const doLogin = async (event) => {
     event.preventDefault();
     var obj = { login: loginName.value, password: loginPassword.value };
     var js = JSON.stringify(obj);
     try {
-      const response = await fetch(buildPath('api/login'), {
-        method: 'POST',
+      const response = await fetch(buildPath("api/login"), {
+        method: "POST",
         body: js,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
       });
       var res = JSON.parse(await response.text());
       if (res.id <= 0) {
-        setMessage('User/Password combination incorrect');
+        setMessage("User/Password combination incorrect");
       } else {
-        var user = { firstName: res.firstName, lastName: res.lastName, id: res.id };
-        localStorage.setItem('user_data', JSON.stringify(user));
-        setMessage('');
-        window.location.href = '/cards';
+        var user = {
+          firstName: res.firstName,
+          lastName: res.lastName,
+          id: res.id,
+        };
+        localStorage.setItem("user_data", JSON.stringify(user));
+        setMessage("");
+        window.location.href = "/cards";
       }
     } catch (e) {
       alert(e.toString());
       return;
     }
-  }
+  };
 
   return (
     <div id="loginDiv">
       <form onSubmit={doLogin}>
-        <span id="inner-title">PLEASE LOG IN</span><br />
-        <input type="text" id="loginName" placeholder="Username"
-          ref={(c) => (loginName = c)} />
-        <input type="password" id="loginPassword" placeholder="Password"
-          ref={(c) => (loginPassword = c)} />
-        <input type="submit" id="loginButton" className="buttons" value="Do It" />
+        <span id="inner-title">PLEASE LOG IN</span>
+        <br />
+        <input
+          type="text"
+          id="loginName"
+          placeholder="Username"
+          ref={(c) => (loginName = c)}
+        />
+        <input
+          type="password"
+          id="loginPassword"
+          placeholder="Password"
+          ref={(c) => (loginPassword = c)}
+        />
+        <input
+          type="submit"
+          id="loginButton"
+          className="buttons"
+          value="Do It"
+        />
       </form>
+      <button onClick={() => props.onFormSwitch("Register")}>
+        Don't have an account? Register here
+      </button>
       <span id="loginResult">{message}</span>
     </div>
   );
-}
+};
 
 export default Login;
