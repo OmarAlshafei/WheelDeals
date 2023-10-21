bc56a69e0292b6e01549a0862c4d52c0642c66f7
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-10-21 11:37:31 -0400

    make navbar appear at all pages

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Slideshow from "../components/Slideshow";
 import Table from "../components/Table";
 
 const HomePage = () => {
   return (
     <div className="HomePage">
-      <Navbar />
       <Slideshow />
       <Table />
     </div>
