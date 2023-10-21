e98ce1b58d0900d45879d7150cb7edafb8805d00
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2023-10-21 15:32:39 -0400

    working on slideshow and table

--- frontend/src/components/Slideshow.js
+++ frontend/src/components/Slideshow.js
@@ -1,54 +1,76 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel  from "react-bootstrap/Carousel";
+import { useState } from 'react';
 
 const Slideshow = () => {
   return (
-    <div
-      id="carouselExampleControls"
-      class="carousel slide"
-      data-mdb-ride="carousel"
-    >
+    <div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-mdb-target="#carouselBasicExample"
+          data-mdb-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-mdb-target="#carouselBasicExample"
+          data-mdb-slide-to="1"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-mdb-target="#carouselBasicExample"
+          data-mdb-slide-to="2"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+
       <div class="carousel-inner">
+    
         <div class="carousel-item active">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-            class="d-block w-100"
-            alt="Wild Landscape"
-          />
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="Sunset Over the City"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
         </div>
+
         <div class="carousel-item">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-            class="d-block w-100"
-            alt="Camera"
-          />
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100" alt="Canyon at Nigh"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
         </div>
+
         <div class="carousel-item">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-            class="d-block w-100"
-            alt="Exotic Fruits"
-          />
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100" alt="Cliff Above a Stormy Sea"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </div>
         </div>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-mdb-target="#carouselExampleControls"
-        data-mdb-slide="prev"
-      >
+
+      <button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-mdb-target="#carouselExampleControls"
-        data-mdb-slide="next"
-      >
+      <button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
+
+
     </div>
   );
 };
