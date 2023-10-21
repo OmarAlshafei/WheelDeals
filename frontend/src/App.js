d27ea2fca8f3da9610f3f2d5165935b3c04f038e
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-10-21 11:37:31 -0400

    make navbar appear at all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+import SigninPage from "./pages/SigninPage";
 import HomePage from "./pages/HomePage";
 import TestPage from "./pages/TestPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/home" component={HomePage} />
-        <Route path="/signin" component={LoginPage} />
+        <Route path="/signin" component={SigninPage} />
         <Route path="/test" component={TestPage} />
       </Switch>
     </Router>
