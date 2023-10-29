6c7bb33a103c114ad054dde5da579fe3b95e6360
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-10-29 14:44:23 -0400

    changed lago, and navbar format

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,12 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import logo from "./main-logo.png";
 import React from "react";
-import Form from 'react-bootstrap/Form';
-
+import Form from "react-bootstrap/Form";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-light">
       <div className="container">
         <a href="/home" className="navbar-brand">
           <img src={logo} height="40" alt="Logo" loading="lazy" />
@@ -15,9 +15,17 @@ const Navbar = () => {
 
         <Form>
           <Form.Group className="mb-3" controlId="searchBar">
-            <Form.Control type="search" placeholder="Search" style={{marginTop: '10px', paddingLeft: '240px', paddingRight: '150px'}} />
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              style={{
+                marginTop: "10px",
+                paddingLeft: "240px",
+                paddingRight: "150px",
+              }}
+            />
           </Form.Group>
-         </Form>
+        </Form>
 
         <ul className="nav nav-pills">
           <li className="nav-item">
