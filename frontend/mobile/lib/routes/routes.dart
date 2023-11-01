70fef792f2e2ded880fff0e9c94136c244081157
bickleral <96271271+bickleral@users.noreply.github.com>
2023-11-01 19:24:26 -0400

    Register

--- frontend/mobile/lib/routes/routes.dart
+++ frontend/mobile/lib/routes/routes.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:mobile/screens/LoginScreen.dart';
 import 'package:mobile/screens/CarsScreen.dart';
 import 'package:mobile/screens/HomeScreen.dart';
+import 'package:mobile/screens/RegisterScreen.dart';
+
 
 class Routes {
   static const String LOGINSCREEN = '/login';
   static const String CARSSCREEN = '/cars';
   static const String HOMESCREEN = '/home';
+  static const String REGISTERSCREEN = '/home';
 
   // routes of pages in the app
   static Map<String, Widget Function(BuildContext)> get getroutes => {
-    '/': (context) => LoginScreen(),
+    '/': (context) => RegisterScreen(),
     LOGINSCREEN: (context) => LoginScreen(),
     CARSSCREEN: (context) => CarsScreen(),
     HOMESCREEN: (context) => HomeScreen(),
+    REGISTERSCREEN: (context) => RegisterScreen(),
   };
 }
---
+++ frontend/mobile/lib/screens/RegisterScreen.dart
@@ -0,0 +1,543 @@
+import 'dart:core';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:mobile/utils/getAPI.dart';
+import 'package:mobile/utils/Colors.dart';
+import 'package:mobile/utils/header.dart';
+import 'package:mobile/utils/currentUser.dart' as currentUser;
+import 'dart:convert';
+import 'package:mobile/routes/routes.dart';
+
+class GlobalData
+{
+  static String userId = '';
+  static String firstName = '';
+  static String lastName = '';
+  static String loginName = '';
+  static String password = '';
+  static String state = '';
+  static String email = '';
+// state, email
+}
+
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, Routes.REGISTERSCREEN);
+          },
+          child: Image.asset(
+              'images/wheel.png',
+              width:70
+          ),
+        ),
+
+        backgroundColor: Colors.black54,
+        title: Text("Wheel Deals"),
+      ),
+      endDrawer: Drawer(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 110, // To change the height of DrawerHeader
+                width: double.infinity, // To Change the width of DrawerHeader
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.amber),
+                  child: Text('Menu',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              /*
+              ListTile(
+                title: const Text('Login'),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Login'),
+                    ),//AlertDialog
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Register'),
+                //login/signup->register
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Register'),
+                      //login/signup->register
+                    ),//AlertDialog
+                  );
+                },
+              ), */
+              ListTile(
+                title: const Text('Login/Signup'),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Login/Signup'),
+                    ),//AlertDialog
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Favorites'),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Favorites'),
+                    ),//AlertDialog
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Account'),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Account'),
+                    ),//AlertDialog
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Logout'),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Logout'),
+                    ),//AlertDialog
+                  );
+                },
+              ),
+            ],
+          )
+      ),
+      backgroundColor: Colors.white,
+      body: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  String message = " ", newMessageText = '';
+  String loginName = '', password = '';
+  String firstName = '', lastName = '';
+  String state = '', email = '';
+
+
+
+  Color messageColor = appColors.black;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  changeText() {
+    setState(() {
+      message = newMessageText;
+    });
+  }
+
+  /*
+  return new Container(
+  child: new SingleChildScrollView(
+    child: new Column(
+      children: <Widget>[
+        _showChild1(),
+        _showChild2(),
+        ...
+        _showChildN()
+      ]
+    )
+  )
+);
+   */
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+        width: 600,
+        margin:const EdgeInsets.only(bottom: 56.0),
+
+        child: SingleChildScrollView(
+
+        child :Column(
+          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+          //crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontal
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // Container(
+            //   margin: const EdgeInsets.only(bottom:16),
+            //
+            //   child: Image.asset(
+            //       'images/chevy_camero.jpg',
+            //       //width: 70000,
+            //       height: 250,
+            //       fit:BoxFit.fill
+            //   ),
+            // ),
+            Row(
+              children: [
+                Container(
+                  margin:const EdgeInsets.only(left: 55.0),
+                  child:
+                  Text(
+                    "Welcome to Wheel Deals!",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ],
+            ),
+
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin:const EdgeInsets.only(left: 95.0, top:20),
+                    child:
+                    TextField (
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: 'First name',
+                          hintText: 'Enter Your first name'
+                      ),
+                      onChanged: (text) {
+                        firstName = text;
+                      },
+                    ),
+                  ),
+                ]
+            ),
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin:const EdgeInsets.only(left: 95.0, top:20),
+                    child:
+                    TextField (
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: 'Last name',
+                          hintText: 'Enter Your last name'
+                      ),
+                      onChanged: (text) {
+                        lastName = text;
+                      },
+                    ),
+                  ),
+                ]
+            ),
+
+
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin:const EdgeInsets.only(left: 95.0, top:20),
+                    child:
+                    TextField (
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: 'Username',
+                          hintText: 'Enter Your Username'
+                      ),
+                      onChanged: (text) {
+                        loginName = text;
+                      },
+                    ),
+                  ),
+                ]
+            ),
+
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin:const EdgeInsets.only(top: 10.0, left:95.0),
+                    child:
+                    TextField (
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          hintText: 'Enter Your Password'
+                      ),
+                      onChanged: (text) {
+                        password = text;
+                      },
+                    ),
+                  ),
+                ]
+            ),
+
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin:const EdgeInsets.only(left: 95.0, top:20),
+                    child:
+                    TextField (
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: 'State',
+                          hintText: 'Enter Your state'
+                      ),
+                      onChanged: (text) {
+                        state = text;
+                      },
+                    ),
+                  ),
+                ]
+            ),
+
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin:const EdgeInsets.only(left: 95.0, top:20),
+                    child:
+                    TextField (
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                          hintText: 'Enter Your email'
+                      ),
+                      onChanged: (text) {
+                        email = text;
+                      },
+                    ),
+                  ),
+                ]
+            ),
+
+
+
+
+            Row(
+              children: <Widget>[
+                Container(
+                  margin:const EdgeInsets.only(left: 160.0),
+                  child:
+                  ElevatedButton(
+                    child: Text('Register',style: TextStyle(fontSize: 14 ,color:Colors.black)),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: appColors.gold,
+                    ),
+                    onPressed: () async
+                    {
+                      newMessageText = "";
+                      changeText();
+
+                     // String payload = '{"userName":"' + loginName.trim() + '","password":"' + password.trim() + '"}';
+                      String payload = '{"firstName":"' + firstName.trim() + '","userName":"' + loginName.trim() + '","password":"' + password.trim() + '","state":"' + state.trim() + '","email":"' + email.trim() + '"}';
+                      var userId = '';
+                      var fname ='-';
+                      var jsonObject;
+                      String ret = 'ah';
+
+                      if (firstName=='') {
+                        newMessageText = "Please enter first name";
+                        messageColor = appColors.errRed;
+                        changeText();
+
+                      }
+                      if (lastName=='') {
+                        newMessageText = "Please enter last name";
+                        messageColor = appColors.errRed;
+                        changeText();
+
+                      }
+                      if (loginName=='') {
+                        newMessageText = "Please enter Username";
+                        messageColor = appColors.errRed;
+                        changeText();
+
+                      }
+                      if (password=='') {
+                        newMessageText = "Please enter password";
+                        messageColor = appColors.errRed;
+                        changeText();
+
+                      }
+
+                      if (state=='') {
+                        newMessageText = "Please enter state";
+                        messageColor = appColors.errRed;
+                        changeText();
+
+                      }
+                      if (email=='') {
+                        newMessageText = "Please enter email";
+                        messageColor = appColors.errRed;
+                        changeText();
+
+                      }
+
+                      try
+                      {
+                        String url = 'https://wheeldeals-d3e9615ad014.herokuapp.com/api/login';
+                        ret = await CarsData.getJson(url, payload);
+                        jsonObject = json.decode(ret);
+                        fname = jsonObject["firstName"];
+                      }
+                      catch(e)
+                      {
+                        newMessageText = e.toString();
+                        changeText();
+                        return;
+                      }
+                      if (fname == '')
+                      {
+
+                        if (firstName == '' ) {
+                          newMessageText = "Please enter first name";
+                        }
+                        else if (lastName == '') {
+                          newMessageText = "Please enter last name";
+                        }
+                        else if (loginName == '') {
+                          newMessageText = "Please enter Username";
+                        }
+                        else if (password == '') {
+                          newMessageText = "Please enter Password";
+                        }
+
+                        /*
+                        if (loginName == '' && password == '') {
+                          newMessageText = "Please enter Username and Password";
+                        }
+                        else if (loginName == '') {
+                          newMessageText = "Please enter Username";
+                        }
+                        else if (password == '') {
+                          newMessageText = "Please enter Password";
+                        }
+                        else {
+                          newMessageText = "Incorrect Login/Password";
+                        }
+                        */
+
+                        messageColor = appColors.errRed;
+                        changeText();
+                      }
+                      else
+                      {
+                        currentUser.userId = jsonObject["_id"];
+                        currentUser.fName = jsonObject["firstName"];
+                        currentUser.lName = jsonObject["lastName"];
+                        currentUser.email = jsonObject["email"];
+                        currentUser.state = jsonObject["state"];
+                        currentUser.userName = loginName;
+                        currentUser.password = password;
+                        //currentUser.state = state;
+                        //currentUser.email = email;
+                        Navigator.pushNamed(context, Routes.HOMESCREEN);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+            // Row(
+            //   children: <Widget>[
+            //     Container(
+            //       margin: const EdgeInsets.only(left: 105.0),
+            //       child: Text(
+            //           message,
+            //           style: const TextStyle(fontSize: 14 ,color:appColors.errRed),
+            //           textAlign: TextAlign.center
+            //       ),
+            //     )
+            //   ],
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    margin:const EdgeInsets.only(top: 15.0),
+                    child:
+                    Text(
+                      message,
+                      style: TextStyle(fontSize: 18, color: messageColor),
+                    )
+                ),
+              ],
+            ),
+            /*
+            Row(
+              children: [
+                Container(
+                  margin:const EdgeInsets.only(left: 120.0, top: 20),
+                  child:
+                  ElevatedButton(
+                    child: Text('Create an Account',style: TextStyle(fontSize: 14 ,color:Colors.black)),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: appColors.gold,
+                    ),
+                    onPressed: () async
+                    {
+                      Navigator.pushNamed(context, Routes.CARSSCREEN);
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            */
+          ],
+        )
+      )
+    );
+
+  }
+}
