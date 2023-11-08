ce5084b1d49b879a8b4784e13f98f5a22418b8d3
Hong Thy Nguyen <hongthy3101@gmail.com>
2023-11-08 12:21:19 -0500

    table clickable worked

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import HeartIcon from "./HeartIcon";
 import "./Table.css";
+import { Link, NavLink, useHistory } from "react-router-dom";
 
 const Table = () => {
   const [newCars, setNewCars] = useState([]);
@@ -12,9 +13,10 @@ const Table = () => {
     try {
       const app_name = "wheeldeals-d3e9615ad014";
       const route = "api/homepage";
-      const apiUrl = process.env.NODE_ENV === "production"
-        ? `https://${app_name}.herokuapp.com/${route}`
-        : `http://localhost:9000/${route}`;
+      const apiUrl =
+        process.env.NODE_ENV === "production"
+          ? `https://${app_name}.herokuapp.com/${route}`
+          : `http://localhost:9000/${route}`;
 
       const res = await fetch(apiUrl, {
         method: "POST",
@@ -41,10 +43,23 @@ const Table = () => {
     fetchData();
   }, []);
 
+  const history = useHistory();
+
+  const handleRowClick = (item) => {
+    // Use history.push to navigate to the details page and pass data as state
+    history.push({
+      pathname: "/cardetail",
+      state: { make: item.brand, model: item.model },
+    });
+  };
+
   return (
     <>
-    <h1 className="tableTitle">Top popular cars</h1>
-      <table className="table table-hover align-middle mb-0 bg-white" style={{ marginTop: "30px" }}>
+      <h1 className="tableTitle">Top popular cars</h1>
+      <table
+        className="table table-hover align-middle mb-0 bg-white"
+        style={{ marginTop: "30px" }}
+      >
         <thead className="bg-light">
           <tr>
             <th>Rank</th>
@@ -57,7 +72,7 @@ const Table = () => {
         </thead>
         <tbody>
           {newCars.map((car, index) => (
-            <tr key={index}>
+            <tr key={index} onClick={() => handleRowClick(car)}>
               <td>{car.rank}</td>
               <td>{car.type}</td>
               <td>{car.brand}</td>
