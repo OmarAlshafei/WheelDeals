7da182822e10a997fd677caecd85d0bc6898e46c
omar <omaralshafei02@gmail.com>
2023-11-20 22:04:03 -0500

    completed unit tests

--- server.test.js
+++ server.test.js
@@ -1,59 +1,82 @@
 const app = require('./server'); 
 const request = require('supertest');
-const {MongoClient} = require('mongodb');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-let mongoServer;
-let client;
-    
-    
-    beforeAll(async () => {
-      mongoServer = new MongoMemoryServer();
-      const url = process.env.MONGODB_URL;
-      client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-      await client.connect();
-    });
-    
-    afterAll(async () => {
-      await client.close();
-      await mongoServer.stop();
-  });
   
   describe('Server API tests', () => {
 
   // test('POST /api/register - it should register a new user', async () => {
-  //   const response = await request(server)
+  //   const response = await request(app)
   //     .post('/api/register')
   //     .send({
   //         firstName: 'Test',
   //         lastName: 'User',
-  //         userName: 'testuser',
-  //         email: 'test@example.com',
+  //         userName: 'test_user',
+  //         email: 'testing_@example.com',
   //         password: 'TestPassword@123',
+  //         isVerified: 'true'
   //       });
     
   //     expect(response.status).toBe(200);
   //   });
     
   test('POST /api/login - it should log in an existing user', async () => {
-    const usersCollection = client.db('cop4331').collection('Users');
-    const mockUser = {
-      userName: 'a4',
-      password: 'Abc@12345',
-      isVerified: true,
-    };
-    await usersCollection.insertOne(mockUser);
     
     const res = await request(app)
       .post('/api/login')
       .send({ 
-        userName: 'a4', 
-        password: 'Abc@12345' 
+        userName: 'a', 
+        password: 'Abcd@1234' 
       });
       
     expect(res.status).toBe(200);
-    // expect(res.body).toHaveProperty('accessToken');
+    expect(res.body).toHaveProperty('accessToken');
 
   });
-
+  
+  test('POST /api/homepage - it should return array of cars', async () => {
+    
+    const res = await request(app)
+      .post('/api/homepage')
+      .send({ 
+        jwtToken: '90554146df2f1df84db1e23697401f1a', 
+      });
+      
+    expect(res.status).toBe(200);
+  });
+  
+  test('POST /api/makes - it should return array of makes', async () => {
+    
+    const res = await request(app)
+      .post('/api/makes')
+      .send({ 
+        jwtToken: '90554146df2f1df84db1e23697401f1a', 
+      });
+      
+    expect(res.status).toBe(200);
+  });
+  
+  test('POST /api/models - it should return array of makes', async () => {
+    
+    const res = await request(app)
+      .post('/api/models')
+      .send({ 
+        make: 'Ford',
+        jwtToken: '90554146df2f1df84db1e23697401f1a', 
+      });
+      
+    expect(res.status).toBe(200);
+  });
+  
+  test('POST /api/search - it should return array of makes', async () => {
+    
+    const res = await request(app)
+      .post('/api/search')
+      .send({ 
+        make: 'Ford',
+        model: 'F-150',
+        jwtToken: '90554146df2f1df84db1e23697401f1a', 
+      });
+      
+    expect(res.status).toBe(200);
+  });
 
 });
